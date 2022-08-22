da7687f1bbdfbdc0ea58f2057786b53b6b9a7b2c
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2022-08-22 18:38:19 +0300

    promise

--- Task/Promise.js
+++ Task/Promise.js
@@ -4,7 +4,7 @@
 //         setTimeout(() => resolve(), ms)
 //     })
 //   }
-  
+
 //   delay(3000).then(() => console.log('выполнилось через 3 секунды'));
 
 //? Перепишите функцию showCircle, написанную в задании Анимация круга с помощью колбэка таким образом, чтобы она возвращала промис, вместо того чтобы принимать в аргументы функцию-callback.
@@ -13,3 +13,47 @@
 //     div.classList.add('message-ball');
 //     div.append("Hello, world!");
 //   });
+
+//? Перепишите один из примеров раздела Цепочка промисов, используя async/await вместо .then/catch:
+async function loadJson(url) {
+  return fetch(url)
+  .then(response => {
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new Error(response.status);
+    }
+  })
+
+}
+loadJson('no-such-user.json') // (3)
+  .catch(alert); // Error: 404
+
+// function one() { console.log(1); }
+// function two() {
+//  promise = fetch('http://giool.ecom').then((Response) =>Response.json())
+// return promise
+
+// }
+// function three(json) {
+// console.log(3)
+// return 'omise'
+// }
+
+// function four(param) {
+//   console.log(4,param);
+// }
+
+// one()
+
+// two()
+// .then(three)
+// .then(four)
+
+// five()
+
+
+
+
+  
+
