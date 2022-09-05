6d831642ca01264790753b634593da9034a1135c
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2022-09-05 21:00:49 +0300

    promise

--- Task/базовое/Promise.js
+++ Task/базовое/Promise.js
@@ -50,34 +50,37 @@ class HttpError extends Error {
   }
 }
 
-function loadJson(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new HttpError(response);
-      }
-    })
+async function loadJson(url) {
+  let response = await fetch(url)
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new HttpError(response);
+    }
 }
 
 // Запрашивать логин, пока github не вернёт существующего пользователя.
-function demoGithubUser() {
-  let name = prompt("Введите логин?", "iliakan");
+async function demoGithubUser() {
+  let user;
+  while(true) {
+    let name = prompt("Введите логин?", "iliakan");
 
-  return loadJson(`https://api.github.com/users/${name}`)
-    .then(user => {
-      alert(`Полное имя: ${user.name}.`);
-      return user;
-    })
-    .catch(err => {
+    try {
+      user = await loadJson(`https://api.github.com/users/${name}`);
+      break; // ошибок не было, выходим из цикла
+    } catch(err) {
       if (err instanceof HttpError && err.response.status == 404) {
+        // после alert начнётся новая итерация цикла
         alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
-        return demoGithubUser();
       } else {
+        // неизвестная ошибка, пробрасываем её
         throw err;
       }
-    });
+    }
+  }
+
+  alert(`Полное имя: ${user.name}.`);
+  return user;
 }
 
 demoGithubUser();
